2fe021bf92d661aa379d62485fac90559ae9699d
Ikonera <gabrielmlt@protonmail.ch>
2023-01-05 18:21:28 +0100

    feat(formatter): add dockerfile-ts

--- lua/ikonera/treesitter.lua
+++ lua/ikonera/treesitter.lua
@@ -1,22 +1,20 @@
 local ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not ok then
-  print("[Treesitter] plugin require error.")
+	print("[Treesitter] plugin require error.")
 end
 
 treesitter.setup({
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  ensure_installed = {
-    "lua",
-    "typescript",
-    "tsx",
-    "javascript",
-    "json",
-    "yaml",
-  },
-  auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	ensure_installed = {
+		"lua",
+		"typescript",
+		"tsx",
+		"javascript",
+		"json",
+		"yaml",
+		"dockerfile",
+	},
+	auto_install = true,
 })
