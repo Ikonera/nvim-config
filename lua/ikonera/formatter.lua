873aa89d767527d71030b1c355b426331fba0cc9
Ikonera <gabrielmlt@protonmail.ch>
2023-01-05 12:50:51 +0100

    feat(format): add format on save

--- lua/ikonera/formatter.lua
+++ lua/ikonera/formatter.lua
@@ -3,6 +3,36 @@ if not ok then
 	print("[Formatter] plugin require error.")
 end
 
+local ok, mason_null_ls = pcall(require, "mason-null-ls")
+if not ok then
+	print("[Formatter] mason-null-ls require error.")
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+mason_null_ls.setup({
+	ensure_installed = {
+		"prettierd",
+		"stylua",
+	},
+})
+
 null_ls.setup({
-	sources = { null_ls.builtins.formatting.prettierd }
+	sources = {
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.stylua,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,60 +1,72 @@
 local ok, packer = pcall(require, "packer")
 if not ok then
-  print("Packer require error.")
+	print("Packer require error.")
 end
 
-packer.startup(function(use)
-  use "wbthomason/packer.nvim"
+packer.startup({
+	function(use)
+		use("wbthomason/packer.nvim")
 
-  use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  }
+		use({
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		})
 
-  use "windwp/nvim-autopairs"
+		use("windwp/nvim-autopairs")
 
-  use {
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip"
-  }
+		use({
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		})
 
-  use "EdenEast/nightfox.nvim"
+		use("EdenEast/nightfox.nvim")
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = { { "nvim-lua/plenary.nvim" } },
+		})
 
-  -- Telescope extensions
-  use {
-    "ghassan0/telescope-glyph.nvim",
-    "nvim-telescope/telescope-file-browser.nvim",
-  }
+		-- Telescope extensions
+		use({
+			"ghassan0/telescope-glyph.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		})
 
-  use "nvim-treesitter/nvim-treesitter"
+		use("nvim-treesitter/nvim-treesitter")
 
-  use "lewis6991/gitsigns.nvim"
+		use("lewis6991/gitsigns.nvim")
 
-  use "tamton-aquib/staline.nvim"
+		use("tamton-aquib/staline.nvim")
 
-  use "folke/neodev.nvim"
+		use("folke/neodev.nvim")
 
-  use "rcarriga/nvim-notify"
+		use("rcarriga/nvim-notify")
 
+		use("nvim-tree/nvim-web-devicons")
 
-  use "nvim-tree/nvim-web-devicons"
+		use("onsails/lspkind.nvim")
 
-  use "onsails/lspkind.nvim"
+		use({
+			"jackMort/ChatGPT.nvim",
+			requires = "MunifTanjim/nui.nvim",
+		})
 
-  use "jose-elias-alvarez/null-ls.nvim"
+		use("jose-elias-alvarez/null-ls.nvim")
 
-end)
+		use("jayp0521/mason-null-ls.nvim")
+	end,
+	config = {
+		display = {
+			open_fn = require("packer.util").float,
+		},
+	},
+})
 
 vim.cmd([[
   augroup packer_user_config
