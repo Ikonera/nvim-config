3b5d7b8106610b979b0821d9d3f55fb7e4be5f1f
Ikonera <gabrielmlt@protonmail.ch>
2023-01-05 13:08:56 +0100

    style: format files

--- init.lua
+++ init.lua
@@ -1,69 +1,74 @@
 local ok, _ = pcall(require, "plugins")
 if not ok then
-  print("[Init] plugins import error.")
+	print("[Init] plugins import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.mappings")
 if not ok then
-  print("[Init] mappings import error.")
+	print("[Init] mappings import error.")
 end
 
 local ok, _ = pcall(require, "config")
 if not ok then
-  print("[Init] config import error.")
+	print("[Init] config import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.lsp")
 if not ok then
-  print("[Init] lsp import error.")
+	print("[Init] lsp import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.theme")
 if not ok then
-  print("[Init] theme import error.")
+	print("[Init] theme import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.telescope")
 if not ok then
-  print("[Init] telescope import error.")
+	print("[Init] telescope import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.autopairs")
 if not ok then
-  print("[Init] autopairs import error.")
+	print("[Init] autopairs import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.gitsigns")
 if not ok then
-  print("[Init] gitsigns import error.")
+	print("[Init] gitsigns import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.treesitter")
 if not ok then
-  print("[Init] treesitter import error.")
+	print("[Init] treesitter import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.staline")
 if not ok then
-  print("[Init] staline import error.")
+	print("[Init] staline import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.neodev")
 if not ok then
-  print("[Init] neodev import error.")
+	print("[Init] neodev import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.notify")
 if not ok then
-  print("[Init] notify import error.")
+	print("[Init] notify import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.chatgpt")
 if not ok then
-  print("[Init] chatgpt import error.")
+	print("[Init] chatgpt import error.")
 end
 
 local ok, _ = pcall(require, "ikonera.formatter")
 if not ok then
-  print("[Init] formatter import error.")
+	print("[Init] formatter import error.")
+end
+
+local ok, _ = pcall(require, "ikonera.comment")
+if not ok then
+	print("[Init] comment import error.")
 end
--- lua/ikonera/mappings.lua
+++ lua/ikonera/mappings.lua
@@ -1,41 +1,55 @@
 local nkey = vim.keymap.set
 local opts = { noremap = true, silent = true, nowait = true }
 
-  -- basic keymaps
+-- basic keymaps
 
-nkey('n', "<space>w", ":w<CR>", opts)
-nkey('n', "<space>q", ":x<CR>", opts)
-nkey('n', "<space>d", ":bd<CR>", opts)
-nkey('n', "<space>c", ":clo<CR>", opts)
-nkey('n', "<space>t", ":term<CR>", opts)
-nkey('n', "<space>lg", ":term lazygit<CR>", opts)
-nkey('n', "<space>ld", ":term lazydocker<CR>", opts)
+nkey("n", "<space>w", ":w<CR>", opts)
+nkey("n", "<space>q", ":x<CR>", opts)
+nkey("n", "<space>d", ":bd<CR>", opts)
+nkey("n", "<space>c", ":clo<CR>", opts)
+nkey("n", "<space>t", ":term<CR>", opts)
+nkey("n", "<space>lg", ":term lazygit<CR>", opts)
+nkey("n", "<space>ld", ":term lazydocker<CR>", opts)
 
-  -- theprimeagen greatest keymaps
+-- theprimeagen greatest keymaps
 
-nkey('v', 'J', ":m '>+1<CR>gv=gv'")
-nkey('v', 'K', ":m '<-2<CR>gv=gv'")
-nkey('x', '<space>p', "\"_dP")
+nkey("v", "J", ":m '>+1<CR>gv=gv'")
+nkey("v", "K", ":m '<-2<CR>gv=gv'")
+nkey("x", "<space>p", '"_dP')
 
-nkey('n', "<space>r", ":%s/\\<<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>")
+nkey("n", "<space>r", ":%s/\\<<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>")
 
-  -- asbjornHaland greatest keymaps
+-- asbjornHaland greatest keymaps
 
-nkey('v', '<space>y', "\"+y") -- copy things system's clipboard wide
+nkey("v", "<space>y", '"+y') -- copy things system's clipboard wide
 
-  -- telescope
+-- telescope
 
-nkey('n', "ff", ":Telescope find_files hidden=true<CR>", opts)
-nkey('n', "fb", ":Telescope buffers<CR>", opts)
-nkey('n', "fg", ":Telescope live_grep<CR>", opts)
-nkey('n', "fn", ":Telescope file_browser<CR>", opts)
+nkey("n", "ff", ":Telescope find_files hidden=true<CR>", opts)
+nkey("n", "fb", ":Telescope buffers<CR>", opts)
+nkey("n", "fg", ":Telescope live_grep<CR>", opts)
+nkey("n", "ls", ":Telescope file_browser<CR>", opts)
 
-  -- native lsp
+-- native lsp
 
-nkey('n', "gd", function() vim.lsp.buf.definition() end, opts)
-nkey('n', "gh", function() vim.lsp.buf.hover() end, opts)
-nkey('n', "gr", function() vim.lsp.buf.references() end, opts)
-nkey('n', "ga", function() vim.lsp.buf.code_action() end, opts)
-nkey('n', "R", function() vim.lsp.buf.rename() end, opts)
-nkey('n', "[", function() vim.diagnostic.goto_prev() end, opts)
-nkey('n', "]", function() vim.diagnostic.goto_next() end, opts)
+nkey("n", "gd", function()
+	vim.lsp.buf.definition()
+end, opts)
+nkey("n", "gh", function()
+	vim.lsp.buf.hover()
+end, opts)
+nkey("n", "gr", function()
+	vim.lsp.buf.references()
+end, opts)
+nkey("n", "ga", function()
+	vim.lsp.buf.code_action()
+end, opts)
+nkey("n", "R", function()
+	vim.lsp.buf.rename()
+end, opts)
+nkey("n", "[", function()
+	vim.diagnostic.goto_prev()
+end, opts)
+nkey("n", "]", function()
+	vim.diagnostic.goto_next()
+end, opts)
