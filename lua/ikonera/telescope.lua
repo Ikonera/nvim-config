7a1f77434f153a9485bed21fef74372506e3848e
Ikonera <gabrielmlt@protonmail.ch>
2023-01-05 13:08:38 +0100

    feat(telescope): change some keymaps

--- lua/ikonera/telescope.lua
+++ lua/ikonera/telescope.lua
@@ -1,43 +1,45 @@
 local ok, telescope = pcall(require, "telescope")
 if not ok then
-  print("[Telescope] plugin require error.")
+	print("[Telescope] plugin require error.")
 end
 
 local fb_actions = telescope.extensions.file_browser.actions
 
 telescope.setup({
-  defaults = {
-    sorting_strategy = "ascending",
-    selection_caret = " ",
+	defaults = {
+		sorting_strategy = "ascending",
+		selection_caret = " ",
 		dynamic_preview_title = true,
-    file_ignore_patterns = {
-      ".git/",
-      "node_modules/",
-      ".yarn/cache/",
-    }
-  },
-  pickers = {
-    find_files = {
-      prompt_prefix = "$ ",
+		file_ignore_patterns = {
+			".git/",
+			"node_modules/",
+			".yarn/cache/",
+		},
+	},
+	pickers = {
+		find_files = {
+			prompt_prefix = "$ ",
 			results_title = "Found files",
-			prompt_title = "What are you searching for ?"
-    },
-    live_grep = {
-      prompt_prefix = "  "
-    }
-  },
-  extensions = {
-    file_browser = {
-      initial_mode = "normal",
-      mappings = {
-        n = {
-          r = fb_actions.rename,
-	        p = fb_actions.create_from_prompt,
-          ['~'] = fb_actions.goto_home_dir,
-        }
-      }
-    }
-  }
+			prompt_title = "What are you searching for ?",
+		},
+		live_grep = {
+			prompt_prefix = "  ",
+		},
+	},
+	extensions = {
+		file_browser = {
+			initial_mode = "normal",
+			mappings = {
+				n = {
+					r = fb_actions.rename,
+					["~"] = fb_actions.goto_home_dir,
+				},
+				i = {
+					["<CR>"] = fb_actions.create_from_prompt,
+				},
+			},
+		},
+	},
 })
 
 telescope.load_extension("glyph")
