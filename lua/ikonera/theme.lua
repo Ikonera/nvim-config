ada1a37e078142c0949a77e2f84f3bb66edffcf6
Ikonera <gabrielmlt@protonmail.ch>
2023-01-17 13:32:03 +0100

    style: format file

--- lua/ikonera/theme.lua
+++ lua/ikonera/theme.lua
@@ -1,25 +1,25 @@
 local ok, nightfox = pcall(require, "nightfox")
 if not ok then
-  print("[Theme] nightfox require error.")
+	print("[Theme] nightfox require error.")
 end
 
 nightfox.setup({
-  options = {
-    transparent = true,
-    terminal_colors = true,
-    styles = {
-      comments = "italic",
-      conditionals = "",
-      constants = "italic,bold",
-      functions = "",
-      keywords = "bold",
-      numbers = "",
-      operators = "",
-      strings = "",
-      types = "",
-      variables = ""
-    }
-  }
+	options = {
+		transparent = true,
+		terminal_colors = true,
+		styles = {
+			comments = "italic",
+			conditionals = "",
+			constants = "italic,bold",
+			functions = "",
+			keywords = "bold",
+			numbers = "",
+			operators = "",
+			strings = "",
+			types = "",
+			variables = "",
+		},
+	},
 })
 
 vim.cmd("colorscheme nightfox")
