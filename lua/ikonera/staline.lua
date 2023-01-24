5c28f63872f4da43e0e7157fada42eaebf87fee3
Ikonera <gabrielmlt@protonmail.ch>
2023-01-24 10:49:36 +0100

    feat(staline): config plugin

--- lua/ikonera/staline.lua
+++ lua/ikonera/staline.lua
@@ -1,8 +1,10 @@
 local ok, staline = pcall(require, "staline")
 if not ok then
-  print("[Staline] plugin require error")
+	print("[Staline] plugin require error")
 end
 
 staline.setup({
-
+	sections = {
+		left = { "- ", "-mode", "left_sep_double", "lsp", "branch" },
+	},
 })
